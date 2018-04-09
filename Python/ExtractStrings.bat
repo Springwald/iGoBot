8112999d4deb15c7c5ff89f845f7f666747d0f72
Daniel Springwald <daniel@springwald.de>
2018-04-09 20:07:25 +0200

    changed i18n to utf8

--- Python/ExtractStrings.bat
+++ Python/ExtractStrings.bat
@@ -1,2 +1,2 @@
-py  "c:\Program Files (x86)\Python36-32\Tools\i18n\pygettext.py"  -d iGoBot iGoBot.py
+py -3.6 "c:\Program Files (x86)\Python36-32\Tools\i18n\pygettext.py"  -d iGoBot iGoBot.py
 pause
